aba879b133c5d746d797732121058d0aa30f6f13
Ubuntu <azureuser@azuretest.jinyal3qsxvefcbjrn4occnqsb.bx.internal.cloudapp.net>
2022-12-23 18:33:20 +0000

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='my-package',
     version='1.0.0',
-    packages=['my_package'],
+    packages=find_packages(),
     install_requires=[
           'fastapi'
           'uvicorn',
